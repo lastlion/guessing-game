d4fe690c41937e8d08375ca95dff97ba8e2b0872
Sergey Muntyanu <lastlionemail@gmail.com>
2017-09-14 03:46:23 +0300

    Implemented GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0
+        this.max = 0
+        this.gs = 0
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        let avarege = this.max - this.min
+        this.gs = Math.round(this.min + avarege / 2)
+        return this.gs
     }
 
     lower() {
-
+        this.max = this.gs
     }
 
     greater() {
-
+        this.min = this.gs
     }
 }
 
